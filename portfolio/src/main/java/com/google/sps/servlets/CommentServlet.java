f4a3684ada4d521d970f1970492c0e45b535667f
Orpaz <orpaz102@gmail.com>
2020-08-24 14:11:18 +0000

    add a new servlet to enable sending hard-coded comments as JSON

--- portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
@@ -17,31 +17,32 @@ package com.google.sps.servlets;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-  private List<String> facts;
+/** Servlet that handle comments data. */
+@WebServlet("/comment")
+public class CommentServlet extends HttpServlet {
+  private List<String> comments;
 
   @Override
   public void init() {
-      facts = new ArrayList<>();
+      comments = new ArrayList<>();
 
-      facts.add("I'm one of a triplet - and we are all females");
-      facts.add("My big brothers are twins");
-      facts.add("The names of my entire family start with 'א'");
-      facts.add("I have played handball for 8 years, 13 games seasons");
+      comments.add("You are the best!");
+      comments.add("Hello Orpaz");
+      comments.add("Have a nice day");
  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String fact = facts.get((int) (Math.random() * facts.size()));
+        Gson gson = new Gson();
+        String jsonComment = gson.toJson(comments);
 
-    response.setContentType("text/html; charset=UTF-8");
-    response.getWriter().println(fact);
+        response.setContentType("application/json;");
+        response.getWriter().println(jsonComment);
   }
 }
