a03cfc552c6c4c2dd034320637f39110d76c8f7d
Orpaz <orpaz102@gmail.com>
2020-09-07 14:19:52 +0000

    implement FindMeetingQuery according to the tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,53 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
 
 public final class FindMeetingQuery {
+  /**Return all possible time intervals for a meeting, according to its attendees' events.*/
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> meetings = findOptionalTimeForMeeting(events, request);
+    return meetings;
+  }
+
+  /**Return all possible time intervals for a meeting, according to its attendees' events.*/
+  private ArrayList<TimeRange> findOptionalTimeForMeeting(Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> meetings = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> sortedTimeRanges = filterOutIrrelevantMeetings(events, request.getAttendees());
+    int start = TimeRange.START_OF_DAY;
+    int end;
+
+    for (TimeRange eventTime : sortedTimeRanges) {
+      end = eventTime.start();
+        if (request.getDuration() <= end - start) {
+          meetings.add(TimeRange.fromStartEnd(start, end, false));
+        }
+      start = Math.max(start, eventTime.end());
+    }
+    // Add the time interval between the end of the last meeting and the end of the day.
+    end = TimeRange.END_OF_DAY;
+    if (request.getDuration() <= end - start) {
+      meetings.add(TimeRange.fromStartEnd(start, end, true));
+    }
+
+    return meetings;
+  }
+
+  /**Filter out the irrelevant meetings and return an ArrayList of all
+  the time intervals which include at least one of the given attendees.*/
+  private ArrayList<TimeRange> filterOutIrrelevantMeetings(Collection<Event> events, Collection<String> attendees) {
+    ArrayList<TimeRange> sortedTimeRanges = new ArrayList<TimeRange>();
+    for (Event event : events) {
+      if (!Collections.disjoint(event.getAttendees(), attendees)) {
+        sortedTimeRanges.add(event.getWhen());
+      }
+    }
+    Collections.sort(sortedTimeRanges, TimeRange.ORDER_BY_START);
+    return sortedTimeRanges;
   }
 }
